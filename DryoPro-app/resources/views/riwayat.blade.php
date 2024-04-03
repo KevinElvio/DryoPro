1c8c522b4e00fc659df5b562a633db648b98bcf9
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2024-04-03 22:08:28 +0700

    Update FE riwayat

--- DryoPro-app/resources/views/riwayat.blade.php
+++ DryoPro-app/resources/views/riwayat.blade.php
@@ -1,31 +1,35 @@
 @extends('layouts.main')
 
 @section('konten')
-    <div class="flex justify-center mt-14 text-4xl">
+    <div class="flex flex-col w-screen items-center mt-14 text-4xl">
         <h1 class="font-bold">Riwayat</h1>
-    </div>
-    <div class="border-b border-black flex justify-evenly items-center text-2xl font-medium mt-20 mx-1/4 px-96">
-        <h1>Date</h1>
-        <h1>Time</h1>
-    </div>
-    <div class="flex items-center justify-center">
-        <div class="">
-            <div class="mx-10 my-10">
-                <h3 class="text-2xl">2024-12-12 pengeringan selama 02 : 15: 12</h3>
-                <h3 class="text-2xl">2024-12-12 pengeringan selama 02 : 15: 12</h3>
-                <h3 class="text-2xl">2024-12-12 pengeringan selama 02 : 15: 12</h3>
-                <h3 class="text-2xl">2024-12-12 pengeringan selama 02 : 15: 12</h3>
-                <h3 class="text-2xl">2024-12-12 pengeringan selama 02 : 15: 12</h3>
-                <h3 class="text-2xl">2024-12-12 pengeringan selama 02 : 15: 12</h3>
-                <h3 class="text-2xl">2024-12-12 pengeringan selama 02 : 15: 12</h3>
-                <h3 class="text-2xl">2024-12-12 pengeringan selama 02 : 15: 12</h3>
-                <h3 class="text-2xl">2024-12-12 pengeringan selama 02 : 15: 12</h3>
-                <h3 class="text-2xl">2024-12-12 pengeringan selama 02 : 15: 12</h3>
-                <h3 class="text-2xl">2024-12-12 pengeringan selama 02 : 15: 12</h3>
-                <h3 class="text-2xl">2024-12-12 pengeringan selama 02 : 15: 12</h3>
-                <h3 class="text-2xl">2024-12-12 pengeringan selama 02 : 15: 12</h3>
-
-            </div>
+        <div class="border-b border-black flex justify-evenly text-2xl font-medium mt-20 w-1/4 ">
+            <h1 class="mb-5">Date</h1>
+            <h1 class="mb-5">Time</h1>
+        </div>
+        <div class="my-5 flex items-center px-32 text-2xl">
+            <h3 class="mr-10">13/06/24</h3>
+            <h3 class="ml-10">14 : 19: 54</h3>
+        </div>
+        <div class="my-5 flex items-center px-32 text-2xl">
+            <h3 class="mr-10">13/06/24</h3>
+            <h3 class="ml-10">14 : 19: 54</h3>
+        </div>
+        <div class="my-5 flex items-center px-32 text-2xl">
+            <h3 class="mr-10">13/06/24</h3>
+            <h3 class="ml-10">14 : 19: 54</h3>
+        </div>
+        <div class="my-5 flex items-center px-32 text-2xl">
+            <h3 class="mr-10">13/06/24</h3>
+            <h3 class="ml-10">14 : 19: 54</h3>
+        </div>
+        <div class="my-5 flex items-center px-32 text-2xl">
+            <h3 class="mr-10">13/06/24</h3>
+            <h3 class="ml-10">14 : 19: 54</h3>
+        </div>
+        <div class="my-5 flex items-center px-32 text-2xl">
+            <h3 class="mr-10">13/06/24</h3>
+            <h3 class="ml-10">14 : 19: 54</h3>
         </div>
     </div>
 @endsection
