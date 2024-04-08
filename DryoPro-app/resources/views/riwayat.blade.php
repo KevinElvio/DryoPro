91f364badd0b89a4e4f11d0277a726a4db04181b
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2024-04-08 08:27:13 +0700

    add pagination in page history

--- DryoPro-app/resources/views/riwayat.blade.php
+++ DryoPro-app/resources/views/riwayat.blade.php
@@ -7,14 +7,12 @@
             <h1 class="mb-5">Date</h1>
             <h1 class="mb-5">Time</h1>
         </div>
-        @foreach ($riwayat as $item)            
-        <div class="my-5 flex items-center px-32 text-2xl">
-            <h3 class="mr-10">{{ $item->waktu->toDateString()}}</h3>
-            <h3 class="ml-10">{{ $item->waktu->toTimeString() }}</h3>
-        </div>
+        @foreach ($riwayat as $item)
+            <div class="my-5 flex items-center px-32 text-2xl">
+                <h3 class="mr-10">{{ $item->waktu->toDateString() }}</h3>
+                <h3 class="ml-10">{{ $item->waktu->toTimeString() }}</h3>
+            </div>
         @endforeach
-
         {{ $riwayat->links() }}
     </div>
-
 @endsection
